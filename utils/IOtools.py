44d87ace59c2ca44b928058a68edf537545052ed
zted <zhangteddy22@gmail.com>
2016-03-18 14:11:43 +0100

    Adding function to get offset IDs for seleted words

--- utils/IOtools.py
+++ utils/IOtools.py
@@ -4,6 +4,7 @@ and some folder manipulations
 """
 
 import os
+from nltk.corpus import wordnet as wn
 
 def writeToFile(aFile,metricString,offsetID,word,numString):
     aFile.write('-{0}- -{1}- -{2}- {3}\n'.format(metricString, str(offsetID), word, numString))
@@ -14,3 +15,19 @@ def removeAllSubfiles(someFile):
     for f in allFiles:
         os.remove(someFile+'/'+f)
     return
+
+def getUniqueOffsetIDs(inFile):
+    '''
+    :param inFile:file that contains words we want offset IDs for
+    :return: a set containing integers which are offset IDs
+    '''
+    uniqueNums = set([])
+    count = 0
+    with open(inFile) as f:
+        for line in f:
+            count += 1
+            w = line.split("\r")[0]
+            synsets = wn.synsets(w)
+            for s in synsets:
+                uniqueNums.add(s.offset())
+    return uniqueNums
